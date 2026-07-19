b47e9a361b78a8ad0d76449a7092d6c189edd550
ghazalalabed <ghazalalabed307@gmail.com>
2026-07-19 12:45:47 +0300

    solve error

--- app/create-profile/page.jsx
+++ app/create-profile/page.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import createNewOrganisation from "../actions/createNewOrganisation";
-import { Phone } from "@mui/icons-material";
+
 
 export default function OrgForm() {
     const router = useRouter();
