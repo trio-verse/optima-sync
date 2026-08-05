ce3c5cf2749400fc3028d5dd10621b34def5264e
dhuhanasser2002 <dhuhanasser2000.2002@gmail.com>
2026-08-05 16:10:45 +0300

    feature/Clients-create-update-getAllClient

--- app/actions/createNewOrganisation.js
+++ app/actions/createNewOrganisation.js
@@ -2,88 +2,94 @@
 import { cookies } from "next/headers";
 
 export async function createOrganisationProfile(formDataPayload) {
-    try {
-        const cookieStore=await cookies();
-        const token=cookieStore.get("token")?.value;
-        const response = await fetch("https://optima.trio-verse.com/api/v1/organizations", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify(formDataPayload),
-        });
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+    const response = await fetch(
+      "https://optima.trio-verse.com/api/v1/organizations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(formDataPayload),
+      },
+    );
+    console.log(formDataPayload);
 
-        const resdata = await response.json();
+    const resdata = await response.json();
 
-        if (!response.ok) {
-            return {
-                success: false,
-                message: data.message || "Failed to create organisation profile.",
-            };
-        }
-        const newOrgId=resdata.data.id;
-        if(newOrgId){
-            cookieStore.set("organaisationId",newOrgId , {
-                path:"/",
-                maxAge: 60*60*24*7,
-                sameSite:"lax"
-            })
-        }
-
-        return {
-            success: true,
-            data: resdata.data,
-            id:resdata.data.id
-        };
-    } catch (error) {
-        console.error("DEBUG Server Action Error",error)
-        return {
-            success: false,
-            message: "An error occurred while connecting to the server.",
-        };
+    console.log(resdata);
+    if (!response.ok) {
+      return {
+        success: false,
+        message: resdata?.message || "Failed to create organisation profile.",
+      };
     }
+    const newOrgId = resdata.data.id;
+    if (newOrgId) {
+      cookieStore.set("organizationId", newOrgId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+        sameSite: "lax",
+      });
+    }
+
+    return {
+      success: true,
+      data: resdata.data,
+      id: resdata.data.id,
+    };
+  } catch (error) {
+    console.error("DEBUG Server Action Error", error);
+    return {
+      success: false,
+      message: "An error occurred while connecting to the server.",
+    };
+  }
 }
 
 export async function uploadInitialLogo(imageFile) {
-    try {
-        const cookieStore=await cookies();
-        const token=cookieStore.get("token")?.value;
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
 
-        const formData = new FormData();
-        formData.append("logo_url", imageFile);
-        const orgId=cookieStore.get("organaisationId")?.value;
-        if(!orgId){
-            return{
-                success:false,  
-                message:"Organaisation ID not found"
-            }
-        }
-        const response = await fetch(`http://localhost:8000/api/v1/organizations/${orgId}/logo`, {
-            method: "POST",
-            headers:{
-            Authorization: `Bearer ${token}`
-            },
-            body: formData,
-        });
-
-        const resdata = await response.json();
-
-        if (!response.ok) {
-            return {
-                success: false,
-                message: resdata.data.message || "Failed to upload logo.",
-            };
-        }
-
-        return {
-            success: true,
-            logo_url: resdata.data.logo_url,
-        };
-    } catch (error) {
-        return {
-            success: false,
-            message: "An error occurred while uploading the logo.",
-        };
+    const formData = new FormData();
+    formData.append("logo", imageFile);
+    const orgId = cookieStore.get("organizationId")?.value;
+    if (!orgId) {
+      return {
+        success: false,
+        message: "Organaisation ID not found",
+      };
     }
+    const response = await fetch(
+      `https://optima.trio-verse.com/api/v1/organizations/${orgId}/logo`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formData,
+      },
+    );
+    const resdata = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: resdata.data.message || "Failed to upload logo.",
+      };
+    }
+
+    return {
+      success: true,
+      logo_url: resdata.data.logo_url,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "An error occurred while uploading the logo.",
+    };
+  }
 }
--- app/actions/services/industryService.js
+++ app/actions/services/industryService.js
@@ -1,139 +1,132 @@
 "use server";
+const API_BASE_URL = "https://optima.trio-verse.com/api/v1";
 
 import { cookies } from "next/headers";
-export async function creatIndustry(newName,newColor) {
-    try {
-        const cookiesStore=await cookies();
-        const token=cookiesStore.get("token")?.value;
+export async function creatIndustry(newName, newColor) {
+  try {
+    const cookiesStore = await cookies();
+    const token = cookiesStore.get("token")?.value;
 
-        const response = await fetch("", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify({name: newName , color: newColor})
-        })
-        const resdata = await response.json();
-        if (!response.ok) {
-            return {
-                success: false,
-                message: resdata.data.message
-            }
-        }
-
-            return {
-                success: true,
-                data: resdata.data,
-                id: resdata?.data.id
-            }
-        
-
-
-    } catch (error) {
-        console.error("DEBUG server Action Error ",error);
-        return{
-            success:false,
-            message:"An error occurred while connecting to the server"
-        }
+    const response = await fetch(`${API_BASE_URL}/industries`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name: newName, color: newColor }),
+    });
+    const resdata = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: resdata.data.message,
+      };
     }
 
+    return {
+      success: true,
+      data: resdata.data,
+      id: resdata?.data.id,
+    };
+  } catch (error) {
+    console.error("DEBUG server Action Error ", error);
+    return {
+      success: false,
+      message: "An error occurred while connecting to the server",
+    };
+  }
 }
-export async function updateIndustry(id,newName,newColor){
-    const storCookies= await cookies();
-    const token=storCookies.get("token")?.value;
-    try{
-        const response =await fetch("",{
-            method:"PATCH",
-            headers:{
-            "Content-Type":"application/json",
-            Authorization: `Bearer ${token}`
-            },
-            body:JSON.stringify({name:newName,color:newColor})
-        });
+export async function updateIndustry(id, newName, newColor) {
+  const storCookies = await cookies();
+  const token = storCookies.get("token")?.value;
+  try {
+    const response = await fetch(`${API_BASE_URL}/industries/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name: newName, color: newColor }),
+    });
 
-        const resdata=await response.json();
-        if(!response.ok){
-            return{
-                success:false,
-                message:resdata?.data.message
-            }
-        }
-    return{
-        success:true,
-        data:resdata.data
+    const resdata = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: resdata?.data.message,
+      };
     }
-    }catch(error){
-        console.error("DEBUG updateIndustry Error",error);
-        return{
-            success:false,
-            message:"An error while connecting the server"
-        }
-    }
+    return {
+      success: true,
+      data: resdata.data,
+    };
+  } catch (error) {
+    console.error("DEBUG updateIndustry Error", error);
+    return {
+      success: false,
+      message: "An error while connecting the server",
+    };
+  }
 }
 
-export async function deleteIndustry(id){
-    try{
-    const cookieStore=await cookies();
-    const token=cookieStore.get("token")?.value;
-    const response=await fetch("",{
-        method:"DELETE",
-        headers:{
-            "Content-Type":"application/json",
-            Authorization: `Bearer ${token}`
-        },
-    })
-    const resdata=await response.json();
-    if(!response.ok){
-        return{
-            success:false,
-            message:resdata?.data.message
-        }
+export async function deleteIndustry(id) {
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+    const response = await fetch(`${API_BASE_URL}/industries/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const resdata = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: resdata?.data.message,
+      };
     }
-    return{
-        success:true,
-        data:resdata?.data
-    }
-    }catch(error){
-        console.error("DEBUG delete Industry Error",error);
-        return{
-            success:false,
-            message:"error while connecting to server"
-        }
-    }
-
-
+    return {
+      success: true,
+      data: resdata?.data,
+    };
+  } catch (error) {
+    console.error("DEBUG delete Industry Error", error);
+    return {
+      success: false,
+      message: "error while connecting to server",
+    };
+  }
 }
-export async function getIndustry(){
-    try{
-        const cookieStore=await cookies();
-        const token=cookieStore.get("token")?.value;
-        const response=await fetch("",{
-            method:"GET",
-            headers:{
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`
-                
-                
-            },
-            cache:"no-store"
-        })
-        const resdata=await response.json();
-        if(!response.ok){
-            return{
-                success:false,
-                message:resdata?.data.message
-            }
-        }
-        return{
-            success:true,
-            data:resdata?.data
-        }
-    }catch(error){
-        console.error("DEBUG getIndustry Error",error)
-        return{
-            success:false,
-            message:"error while connicting the server"
-        }
+export async function getIndustry() {
+  try {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+    const response = await fetch(`${API_BASE_URL}/industries`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      cache: "no-store",
+    });
+    const resdata = await response.json();
+    if (!response.ok) {
+      return {
+        success: false,
+        message: resdata?.data.message,
+      };
     }
+    return {
+      success: true,
+      data: resdata?.data,
+    };
+  } catch (error) {
+    console.error("DEBUG getIndustry Error", error);
+    return {
+      success: false,
+      message: "error while connicting the server",
+    };
+  }
 }
